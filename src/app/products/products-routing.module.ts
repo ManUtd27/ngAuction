0f5c0443151247152db7209d90e64e82a390a57a
Shawn Williams <jr.william09@gmail.com>
2019-04-04 18:15:02 -0500

    made sure feature module routing was working

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+
+
+const routes: Routes = [
+  {path: 'product', component: ProductDetailComponent}
+];
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
