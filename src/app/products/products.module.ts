fb0c90dfcf687a8819d1fed0eb7c4999fa7fb5cd
Shawn Williams <jr.william09@gmail.com>
2019-04-04 18:14:42 -0500

    made sure feature module routing was working

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {ProductsRoutingModule} from './products-routing.module';
 
 @NgModule({
   declarations: [ProductItemComponent, ProductDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsRoutingModule
   ]
 })
 export class ProductsModule { }
